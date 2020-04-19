8378579636c45a0469bd4510b09ce57a284550b0
renatopcinfo <renatopcinfo@gmail.com>
2020-04-19 17:51:04 -0300

    project-webpack

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './Button';
+import { Card } from './Card';
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Aula Webpack</p>
+      <div>Webpack 4</div>
+      <Button />
+      <Card />
+    </div>
+  );
+};
+export default App;
